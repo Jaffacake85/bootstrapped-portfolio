2013873fabe160513854f173f13901b8bdb81ae5
Kywanzia Martin <Jaffacake85@users.noreply.github.com>
2018-11-18 08:19:49 -0600

    CSS
    Uploaded CSS files

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
-jQuery(document).ready(function($) {
-    setTimeout(function(){
-        $('.trans-grow').addClass('grow');
-    }, 275);
-});
+var love = document.getElementById('your-heart');      // He likes it
+var ring = document.createElement('engagement-ring');  // Put a ring on it
+var lose = document.getElementById('woman').remove();  
 
+if (love == true) {
+    document.getElementById('her-ring-finger'); {
+        document.getElementById('her-ring-finger') + ring;
+    } 
+} else {
+    document.getElementById('woman') = lose;
+}
 
